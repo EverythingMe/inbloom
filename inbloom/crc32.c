e54bfb9e613db85a160384e33cb6d94aae5ed372
Roey Berman <roey@everything.me>
2015-07-26 11:44:14 +0300

    fix setup.py

--- inbloom/crc32.c
+++ inbloom/crc32.c
@@ -40,6 +40,9 @@
  * CRC32 code derived from work by Gary S. Brown.
  */
 
+#include <stdint.h>
+#include <stdlib.h>
+
 static uint32_t crc32_tab[] = {
 	0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f,
 	0xe963a535, 0x9e6495a3,	0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
