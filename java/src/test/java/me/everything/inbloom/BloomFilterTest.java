3cea70ba11eaa64818b9c661a0bee1576fe3b771
Mario Gonzalez Macedo <omgm@users.noreply.github.com>
2015-08-03 17:55:57 -0700

    New tests for bad and edge-case parameters.
    Applied [mutation testing](http://confreaks.tv/videos/mwrc2014-re-thinking-regression-testing) with Ortask Mutator 1.9. Improved the quality of the tests (and the code) by 13% (from a score of 84.6% to 95.3%).
    
    - Added needed tests to document behavior with bad and edge-case parameters.

--- java/src/test/java/me/everything/inbloom/BloomFilterTest.java
+++ java/src/test/java/me/everything/inbloom/BloomFilterTest.java
@@ -9,6 +9,102 @@ import java.io.InvalidObjectException;
  */
 public class BloomFilterTest extends TestCase {
 
+    public void testCreateFilterWithBadParameters()
+    {
+        try 
+        {
+            BloomFilter_mutated bf = new BloomFilter_mutated( -1, 0.01 );
+            fail( "should have thrown an exception" );
+        }
+        catch( RuntimeException e ) 
+        {
+            assertEquals( "Invalid params for bloom filter", e.getMessage() );
+        }
+        
+        //
+        // Error value cannot be arbitrarily close to zero. Must be bounded.
+        // 
+        try 
+        {
+            BloomFilter_mutated bf = new BloomFilter_mutated( 1, 0.000000001 );
+            fail( "should have thrown an exception: error value is not bounded by a precision or tolerance value." );
+        }
+        catch( RuntimeException e ) 
+        {
+            assertEquals( "Invalid params for bloom filter", e.getMessage() );
+        }
+
+        //
+        // Creating an unusable bloom filter (zero bits, zero hashes) should fail.
+        // 
+        try 
+        {
+            BloomFilter_mutated bf = new BloomFilter_mutated(199, 1.0);
+            fail( "should have thrown an exception: created an unusable bloom filter with zero bits and zero hashes." );
+        }
+        catch( RuntimeException e ) 
+        {
+            assertEquals( "Invalid params for bloom filter", e.getMessage() );
+        }
+
+        //
+        // Giving it an error value that is too big should fail.
+        // 
+        try 
+        {
+            BloomFilter_mutated bf = new BloomFilter_mutated(199, 100.0);
+            fail( "should have thrown an exception: error value is too big." );
+        }
+        catch( RuntimeException e ) 
+        {
+            assertEquals( "Invalid params for bloom filter", e.getMessage() );
+        }
+
+        //
+        // Adding more data than expected should fail.
+        // 
+        try 
+        {
+            byte []data = "add more entries than bytes available".getBytes();
+            BloomFilter_mutated bf0 = new BloomFilter_mutated(data, 1, 0.1); 
+            fail( "should have thrown an exception: too much data." );
+        }
+        catch( RuntimeException e ) 
+        {
+            assertEquals( "Expected 1 bytes, got 37", e.getMessage() );
+        }
+
+    }
+    
+
+    public void testValuesFromPublicAPI()
+    {
+        BloomFilter_mutated bf = null;
+        assertEquals(0.000000001, bf.errorPrecision);
+        
+        bf = new BloomFilter_mutated(1, 0.01);
+        assertEquals(2, bf.bytes);
+        assertEquals(1, bf.entries);
+        assertEquals(0.01, bf.error);
+        assertEquals(9, bf.bits);
+        assertEquals(7, bf.hashes);
+        //
+        bf = new BloomFilter_mutated(1, 0.1);
+        assertEquals(1, bf.bytes);
+        assertEquals(1, bf.entries);
+        assertEquals(0.1, bf.error);
+        assertEquals(4, bf.bits);
+        assertEquals(4, bf.hashes);        
+        //
+        bf = new BloomFilter_mutated(8, 0.000001);
+        assertEquals(29, bf.bytes);
+        assertEquals(8, bf.entries);
+        assertEquals(0.000001, bf.error);
+        assertEquals(230, bf.bits);
+        assertEquals(20, bf.hashes);        
+    }
+
+    
     public void testFilter() throws InvalidObjectException {
         BloomFilter bf = new BloomFilter(20, 0.01);
 
